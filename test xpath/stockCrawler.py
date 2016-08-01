9d3c437e44c32e97e07ef69600f5035d24fc76e9
no limits but...sky <sky.wu.gss@gmail.com>
2016-08-01 17:49:25 +0800

    封裝helper class for get_tse data

--- test xpath/stockCrawler.py
+++ test xpath/stockCrawler.py
@@ -49,11 +49,13 @@ HEADER_RATE_105 = HEADLINE_RATE_105.split(',')
 XPATH_HEADER = '//*[@id="example"]/thead/tr/th/text()' # for 'http://stock.wespai.com/rate{}'
 XPATH_BODY = '//*[@id="example"]/tbody/tr'   # for
 
-def ff_parse(x):
-    return(lambda x: web_utils.get_text(x))
+_CONVERT_ZERO = ['', '--', '---', 'x', 'X', 'null', 'NULL']  # convert illegal value into 0
+
+def clean_number(x):
+    return '0' if (x in _CONVERT_ZERO) else re.sub(",", "", x.strip(""))
 
 class stockCrawler():
-    def  __init__(self, url=None, xheader=None, xbody=None, outfile=None):
+    def  __init__(self, url=None, xheader=None, xbody=None, outfile=None, fn_clean=None):
         year = 105
         self.url = url if (url is not None) else URL_RATE.format(year)
         self.outfile= outfile if (outfile is not None) else CSV_RATE.format(year)
@@ -63,6 +65,10 @@ class stockCrawler():
         self.header = None
         self.xpath_header = xheader if (xheader is not None) else XPATH_HEADER
         self.xpath_body = xbody if (xbody is not None) else XPATH_BODY
+        self.fn_clean =  fn_clean if (fn_clean is not None) else self._clean
+
+    def _clean(self, x):
+        return(x)
 
     def get_doc(self, url=None, infile=None):
         url = url if (url is not None) else self.url
@@ -84,7 +90,7 @@ class stockCrawler():
 
     def get_body(self, xbody=None):
         xbody = xbody if (xbody is not None) else self.xpath_body
-        f_parse = lambda x: web_utils.get_text(x)
+        f_parse = lambda x: self.fn_clean(web_utils.get_text(x))
         #if self.doc is None: _
 
         elist = self.doc.xpath(xbody)  # //*[@id="example"]/tbody/tr[1]/td[2]  --
@@ -95,13 +101,16 @@ class stockCrawler():
         #etl.tocsv(self.rows, self.outfile, encoding='utf8')
         return (self.rows)
 
+    def get_table(self):
+        self.get_doc()
+        self.get_header()
+        return self.get_body()
+
     def save(self):
         etl.tocsv(self.rows, self.outfile, encoding='utf8')
 
     def run(self):
-        self.get_doc()
-        self.get_header()
-        self.get_body()
+        self.get_table()
         self.save()
         #logging.debug('\n{}'.format(self.rows))
 
@@ -113,6 +122,47 @@ def get_wespai(url, outfile):
 def get_exwright(year=105):
     url, outfile = URL_RATE.format(year), CSV_RATE.format(year)
     get_wespai(url, outfile)
+
+def get_tse(date_str='20160701'):
+    outdate = to_taiwan_date(date_str)
+    url = "http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php?download=&qdate={}&selectType=ALL".format(outdate)
+    outfile = PATH + ('{}-t.csv').format(date_str) #outdate.replace('/', ''))
+    xheader = '//*[@id="main-content"]/table[2]/thead/tr[2]/td/text()'  #'//*[@id="main-content"]/table[2]/thead/tr[2]'
+    xbody = '//table[2]/tbody/tr'  #loop for td to get the table content
+
+    sc = stockCrawler(url=url, xheader=xheader, xbody=xbody, outfile=outfile, fn_clean=clean_number)
+    return sc.get_table()
+
+def date_to_int(date_str='20160712', esc_char='/'):
+    date_str = date_str.replace(esc_char, '')
+    return int(date_str[:4]), int(date_str[4:6]), int(date_str[6:])
+
+def to_taiwan_date(date_str='20160712', esc_char='/'):
+    year, month, day = date_to_int(date_str, esc_char)
+    taiwan_date = '{0}{esc_ch}{1:02d}{esc_ch}{2:02d}'.format(year - 1911, month, day, esc_ch=esc_char)
+    return taiwan_date
+
+_ENGLISH_HEADER = 'symbol_id,name,volume,trans,amount,open,high,low,close,sign,change,af_buy,af_buy_amount,af_sell, af_sell_amout,pe'.split(',')
+_HEADER_LINE = 'symbol_id,trade_date,volume,amount,open,high,low,close,change,trans'
+_HEADER = _HEADER_LINE.split(',')
+
+def transform_tse(date_str='20160701'):
+    src_table = get_tse(date_str=date_str)
+
+    dest_table = etl.headers.setheader(src_table, _ENGLISH_HEADER)  # _CHINESE_HEADER) #_ENGLISH_HEADER) # _CHINESE_HEADER)
+    dest_table = etl.cut(dest_table, 0, 1, 4, 5, 6, 7, 8, 9, 10, 3)
+    dest_table = etl.rename(dest_table, 'name', 'trade_date')
+
+    # http://petl.readthedocs.io/en/latest/transform.html#converting-values
+    dest_table = etl.transform.conversions.convert(dest_table
+                                                    , {'trade_date': lambda v, row: date_str
+                                                    #, 'change': lambda v, row: (row.sign + ':' + str(len(row.sign)) + ':' + row.sign[0] + ':' + v)}
+                                                    , 'change': lambda v, row: ('-' + v) if (len(row.sign) == 1 and (row.sign[0] in ['-', u'－'])) else v}
+                                                    , pass_row=True)  # cause _trade_date not worked --> need row values
+
+    dest_table = etl.cutout(dest_table, 'sign')
+    return (dest_table)
+
 
 #orign helper services
 def get_exwright_origin(year=105):
@@ -153,23 +203,20 @@ def test_get_header():
     sc.get_header(xheader=xheader)
     print(sc.header)
 
-def test_get_tse():
-    outdate = '105/07/29'
-    url = "http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX.php?download=&qdate={}&selectType=ALL".format(outdate)
-    outfile = PATH + ('{}-t.csv').format(outdate.replace('/', ''))
-    xheader = '//*[@id="main-content"]/table[2]/thead/tr[2]/td/text()'  #'//*[@id="main-content"]/table[2]/thead/tr[2]'
-    xbody = '//table[2]/tbody/tr'  #loop for td to get the table content
+def test_get_tse(outdate='20160701'):
+    clean_table = get_tse(outdate)
+    dest_table=transform_tse(outdate)
+    print(clean_table)
+    print(dest_table)
 
-    sc = stockCrawler(url=url, xheader=xheader, xbody=xbody, outfile=outfile)
-    sc.run()
-    print(sc.rows)
+    outfile = PATH + ('{}-t.csv').format(outdate.replace('/', ''))
+    etl.tocsv(dest_table, outfile)
 
 def main():
     #test_get_header()
-    test_get_exwright(105)
+    #test_get_exwright(105)
     #test_get_wespai()
-    #test_get_tse()
-    #extend for another criteria to load
+    test_get_tse()
 
 if __name__ == '__main__':
     #logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
